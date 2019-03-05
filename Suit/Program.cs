67edfdd6c019b7c5660a3463ead0f1de82e0841e
Federico Barresi <federico.barresi@evopro-ag.de>
2019-03-05 21:19:15 +0100

    setup ui project and solution

---
+++ Suit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace Suit
+{
+	public class Program
+	{
+		[DllImport("kernel32.dll", SetLastError = true)]
+		private static extern bool FreeConsole();
+
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			FreeConsole();
+
+			var app = new App { ShutdownMode = ShutdownMode.OnLastWindowClose };
+			app.InitializeComponent();
+
+			app.Run();
+
+		}
+	}
+}
